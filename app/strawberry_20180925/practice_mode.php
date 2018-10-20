0d55ac3dfa396530cc03a8b7195ded0e06684f0b
arkadi81 <arkadi81@gmail.com>
2018-10-19 18:50:39 -0700

    automatic push

--- app/strawberry_20180925/practice_mode.php
+++
@@ -1,9 +0,0 @@
-<div class="panel-body text-center" id="practiceIntroContainer" style="display:inline"> <!-- DEPRECATED - change to inline to reinsate -->
-	<div>Practice mode gives you a chance to test yourself with an infinite amount of music theory questions.
-		Once you are finished, click the "Check answers" button above to test yourself. To get a new randomly generated set
-		of questions, just refresh the page (press F5).
-	<br>
-	<a class="navbar-btn btn btn-default" onClick="showFirst()">Begin</a></div>
-</div>
-
-
--- app/strawberry_20180925/tests/logging.php
+++
@@ -1,6 +0,0 @@
-<?php
-	date_default_timezone_set('America/Vancouver');
-
-	$line = date('Y-m-d H:i:s') ." $_SERVER[REMOTE_ADDR]";//.$_SERVER['REQUEST_URI'];
-	file_put_contents('visitors.log', $line . PHP_EOL, FILE_APPEND);
-?>
